30003f4d9baeffad10c745ee685ff7fe2677b9d2
Manu Milesi <manumilesi98@gmail.com>
2024-09-17 14:58:54 -0300

    Agregar archivos de proyecto.

---
+++ KioscoInformaticoApp/KioscoInformaticoApp/ViewModels/IniciarSesionViewModel.cs
@@ -0,0 +1,34 @@
+﻿using KioscoInformaticoApp.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioscoInformaticoApp.ViewModels
+{
+    public class IniciarSesionViewModel : ObjectNotification
+    {
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        private bool recordarContraseña;
+        public bool RecordarContraseña
+        {
+            get { return recordarContraseña;}
+            set { recordarContraseña = value; }
+
+        }
+    }
+}
