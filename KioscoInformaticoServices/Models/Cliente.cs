4e1f6e8dfa5f466068c45ebda9e9125c1ca8fbef
Manu Milesi <manumilesi98@gmail.com>
2024-10-16 18:12:00 -0300

    Refactorización y mejoras en la gestión de clientes
    Se han añadido anotaciones de validación a las propiedades `Nombre`, `Direccion` y `LocalidadId` en la clase `Cliente` en `Cliente.cs` para hacerlas obligatorias.
    
    Se ha añadido una nueva propiedad `Remoto` en `Resources.Designer.cs` para obtener una cadena traducida, y una nueva propiedad `UrlApiLocal` para obtener una URL de API local. También se han añadido las entradas correspondientes en el archivo de recursos `Resources.resx`.
    
    En `GenericService.cs`, se ha modificado la lógica para seleccionar la URL de la API en función del valor de `Remoto`.
    
    En `Clientes.razor`, se ha cambiado la visualización de `LocalidadId` a `Localidad` y se ha actualizado el enlace de edición de cliente. Además, se ha cambiado la navegación para agregar un nuevo cliente a la nueva página `nuevoeditarcliente`.
    
    Se han eliminado los archivos `EditarCliente.razor` y `NuevoCliente.razor`, consolidando su funcionalidad en un nuevo archivo `NuevoEditarCliente.razor`.
    
    En `EditarLocalidad.razor`, `NuevaLocalidad.razor`, `EditarProducto.razor` y `NuevoProducto.razor`, se ha movido el componente `ValidationSummary` para que se muestre después del botón de guardar.
    
    Se ha creado un nuevo archivo `NuevoEditarCliente.razor` que combina la funcionalidad de agregar y editar clientes, incluyendo la carga de localidades y la validación de datos.

--- KioscoInformaticoServices/Models/Cliente.cs
+++ KioscoInformaticoServices/Models/Cliente.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace KioscoInformaticoServices.Models;
 
 public partial class Cliente
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
     public string Nombre { get; set; } = null!;
-
+    [Required(ErrorMessage = "El campo Direccion es obligatorio.")]
     public string Direccion { get; set; } = null!;
 
     public string Telefonos { get; set; } = null!;
 
 
     public DateTime FechaNacimiento { get; set; }
-
+    [Required(ErrorMessage = "El campo Localidad es obligatorio.")]
     public int? LocalidadId { get; set; }
 
     public virtual Localidad? Localidad { get; set; }
